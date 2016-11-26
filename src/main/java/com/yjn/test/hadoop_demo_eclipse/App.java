7e3e2aa1322b0fe91a07e57cf92a24d9bf5f6ba4
yjn <ylittlema@163.com>
2016-11-26 21:50:38 +0800

    first commit

---
+++ src/main/java/com/yjn/test/hadoop_demo_eclipse/App.java
@@ -0,0 +1,13 @@
+package com.yjn.test.hadoop_demo_eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
